4f040ffb89a8988c69c3bd429d60c3fda2d8e2ad
Raulx103 <raulx103@gmail.com>
2024-08-30 19:37:27 +0530

    loginpage added

--- frontend/src/pages/LandingPage.tsx
+++ frontend/src/pages/LandingPage.tsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 function LandingPage() {
   return (
     <div className="w-screen h-screen flex justify-center items-center flex-col gap-4">
-      <h1 className=" text-3xl text-red-700">Welcome</h1>
+      <h1 className="text-3xl text-red-700">Welcome</h1>
       <div className="flex gap-2 ">
         <Button>
           <Link to={"/login"}>Login</Link>
