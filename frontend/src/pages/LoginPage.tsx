8b342f3690eae4bf7e41e1967ee06a56446c9fc2
Raulx103 <raulx103@gmail.com>
2024-08-31 12:18:28 +0530

    form and input component added loginForm ui development finished

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -1,9 +1,42 @@
 import { Button } from "@/components/ui/button";
 import { FcGoogle } from "react-icons/fc";
 import { FaFacebook } from "react-icons/fa";
-import { TypographyH1 } from "@/components/Typography";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { TypographyH2 } from "@/components/Typography";
+import {
+  FormField,
+  FormItem,
+  FormLabel,
+  FormControl,
+  FormMessage,
+  Form,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+
+const loginFormSchema = z.object({
+  email: z.string().email({
+    message: "Invalid email address",
+  }),
+  password: z
+    .string()
+    .min(6, { message: "Username must be at least 6 characters." }),
+});
 
 function LoginPage() {
+  const form = useForm<z.infer<typeof loginFormSchema>>({
+    resolver: zodResolver(loginFormSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  // 2. Define a submit handler.
+  function onSubmit(values: z.infer<typeof loginFormSchema>) {
+    console.log(values);
+  }
   return (
     <div className="flex flex-col py-6 min-h-screen bg-zinc-100">
       <div className="sm:mx-auto sm:w-full sm:max-w-sm">
@@ -12,75 +45,52 @@ function LoginPage() {
           src="https://res.cloudinary.com/dj5yf27lr/image/upload/v1725024672/testAdda/frontendAssets/qetoe2ngulol3glcemfx.png"
           alt="Your Company"
         />
-        <TypographyH1 className="mt-2 text-center">
+        <TypographyH2 className="mt-4 text-center">
           Sign in to your account
-        </TypographyH1>
-        {/* <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-          Sign in to your account
-        </h2> */}
+        </TypographyH2>
       </div>
 
       <div className="mt-10 sm:mx-auto sm:w-2/6 bg-white  border-2 rounded-lg px-12 py-12">
-        <form className="space-y-6" action="#" method="POST">
-          <div>
-            <label
-              htmlFor="email"
-              className="block text-sm font-medium leading-6 text-gray-900"
-            >
-              Email address
-            </label>
-            <div className="mt-2">
-              <input
-                id="email"
-                name="email"
-                type="email"
-                autoComplete="email"
-                required
-                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-              />
-            </div>
-          </div>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <Input placeholder="shadcn" {...field} type="email" />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <FormControl>
+                    <Input placeholder="shadcn" {...field} type="password" />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button type="submit" className="w-full">
+              Submit
+            </Button>
+          </form>
+        </Form>
 
-          <div>
-            <div className="flex items-center justify-between">
-              <label
-                htmlFor="password"
-                className="block text-sm font-medium leading-6 text-gray-900"
-              >
-                Password
-              </label>
-            </div>
-            <div className="mt-2">
-              <input
-                id="password"
-                name="password"
-                type="password"
-                autoComplete="current-password"
-                required
-                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-              />
-            </div>
-            <div className="text-sm flex justify-end items-end mt-4">
-              <a
-                href="#"
-                className="font-semibold text-indigo-600 hover:text-indigo-500"
-              >
-                Forgot password?
-              </a>
-            </div>
-          </div>
-
-          <div>
-            <Button className="w-full">Sign in</Button>
-          </div>
-        </form>
-
-        <div className="flex mt-10 gap-6 justify-center items-center">
+        <div className="flex mt-8 gap-6 justify-center items-center">
           <hr className="h-[1px] w-full bg-gray-300" />
           <p className="w-full text-center text-sm">Or Continue with</p>
           <hr className="h-[1px] w-full bg-gray-300" />
         </div>
-        <div className="flex justify-center items-center gap-2 mt-10">
+        <div className="flex justify-center items-center gap-2 mt-8">
           <Button variant="outline" className="border-2 w-2/4">
             <FcGoogle className="mr-2 h-6 w-6" />
             Google
@@ -91,15 +101,6 @@ function LoginPage() {
           </Button>
         </div>
       </div>
-      {/* <p className="mt-10 text-center text-sm text-gray-500">
-        Not a member?
-        <a
-          href="#"
-          className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
-        >
-          Start a 14 day free trial
-        </a>
-      </p> */}
     </div>
   );
 }
