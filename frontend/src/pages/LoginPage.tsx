09ad44916e68015b63fe9a8574e79fc29b420dbe
Raulx103 <raulx103@gmail.com>
2024-09-10 23:18:14 +0530

    loginPage finishing stage 2 - Ui updated

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -1,10 +1,7 @@
 import { Button } from "@/components/ui/button";
-// import { FcGoogle } from "react-icons/fc";
-// import { FaFacebook } from "react-icons/fa";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
-// import { TypographyH2 } from "@/components/Typography";
 import {
   FormField,
   FormItem,
@@ -15,8 +12,12 @@ import {
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { setAuthenticated, useSendEmailOtpMutation } from "@/store/store";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
+import { FcGoogle } from "react-icons/fc";
+import Logo from "@/components/Logo";
+import { FaDiscord } from "react-icons/fa";
+import { Checkbox } from "@/components/ui/checkbox";
 
 const loginFormSchema = z.object({
   email: z.string().email({
@@ -36,12 +37,10 @@ const LoginPage = () => {
   });
 
   async function onSubmit(values: z.infer<typeof loginFormSchema>) {
-    console.log(values.email);
     try {
       const res = await sendEmailOtp({
         email: values.email,
       });
-      console.log(res);
       if (res.data) {
         dispatch(setAuthenticated());
       }
@@ -53,7 +52,27 @@ const LoginPage = () => {
 
   return (
     <>
-      <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+      <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 flex flex-col gap-6 w-1/4  p-8 rounded-lg">
+        <Logo large />
+        <Button
+          className="w-full flex gap-4 shadow-custom-2 py-6"
+          variant={"outline"}
+        >
+          <FcGoogle className="text-3xl" />
+          Continue With Google
+        </Button>
+        <Button
+          className="w-full flex gap-4 shadow-custom-2 py-6"
+          variant={"outline"}
+        >
+          <FaDiscord className="text-3xl text-[#5865F2]" />
+          Continue With Discord
+        </Button>
+        <div className="flex mt-8 gap-2 justify-center items-center">
+          <hr className="h-[2px] w-full bg-gray-300" />
+          <p className="text-center text-sm text-gray-500">OR</p>
+          <hr className="h-[2px] w-full bg-gray-300" />
+        </div>
         <Form {...form}>
           <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
             <FormField
@@ -64,6 +83,7 @@ const LoginPage = () => {
                   <FormLabel>Email</FormLabel>
                   <FormControl>
                     <Input
+                      className="border-2"
                       placeholder="Enter your email address"
                       {...field}
                       type="email"
@@ -73,11 +93,30 @@ const LoginPage = () => {
                 </FormItem>
               )}
             />
-            <Button type="submit" className="w-full" variant={"lightseagreen"}>
+            <Button
+              type="submit"
+              className="w-full py-6"
+              variant={"lightseagreen"}
+            >
               {results.isLoading ? <>Loading data</> : <>Continue</>}
             </Button>
           </form>
         </Form>
+        <div className="items-top flex space-x-2">
+          <Checkbox id="terms1" />
+          <div className="grid gap-1.5 leading-none">
+            <label
+              htmlFor="terms1"
+              className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+            >
+              By signing in to TestMagister,you agree with our
+            </label>
+            <p className="text-sm text-muted-foreground">
+              <Link to="/terms-&-conditions">Terms & Conditions</Link> and{" "}
+              <Link to="/privacy-policy">Privacy Policy</Link>
+            </p>
+          </div>
+        </div>
       </div>
     </>
   );
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -60,6 +60,12 @@ module.exports = {
         mediumseagreen: "#3AA074",
         bordergray: "#B0B0B0",
       },
+      boxShadow: {
+        "custom-1":
+          "rgba(0, 0, 0, 0.09) 0px 2px 1px, rgba(0, 0, 0, 0.09) 0px 4px 2px, rgba(0, 0, 0, 0.09) 0px 8px 4px, rgba(0, 0, 0, 0.09) 0px 16px 8px, rgba(0, 0, 0, 0.09) 0px 32px 16px;",
+        "custom-2":
+          "rgba(9, 30, 66, 0.25) 0px 4px 8px -2px, rgba(9, 30, 66, 0.08) 0px 0px 0px 1px;",
+      },
       fontFamily: {
         montserrat: ["Montserrat", "sans-serif"],
         inter: ["Inter", "sans-serif"],
