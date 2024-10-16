6ca0544c681418d687aa7cb78a1f871e92dbcdd4
Raulx103 <raulx103@gmail.com>
2024-10-16 20:35:01 +0530

    result model window added

--- frontend/src/components/QuizCard.tsx
+++ frontend/src/components/QuizCard.tsx
@@ -1,4 +1,4 @@
-import { FaLock, FaRocket, FaTimes } from "react-icons/fa";
+import { FaLock, FaRocket, FaTimes, FaTrophy } from "react-icons/fa";
 import { Button } from "./ui/button";
 import { createRoot } from "react-dom/client";
 import {
@@ -10,7 +10,8 @@ import {
   CardContent,
 } from "./ui/card";
 import { QuizData, QuizQuestionsType } from "@/utils/types";
-import store from "@/store/store";
+import { IoAnalytics } from "react-icons/io5";
+import store, { useGetResultMutation } from "@/store/store";
 import { AttemptWindow } from "@/pages/AttemptWindow";
 import { Provider } from "react-redux";
 import UseGetUserDataHook from "@/hooks/UseGetUserDataHook";
@@ -19,6 +20,8 @@ import { RxResume } from "react-icons/rx";
 import Modal from "react-modal";
 import DisplayDate from "./Date";
 import { useState } from "react";
+
+Modal.setAppElement("#root");
 
 const openQuizInNewWindow = (title: string, _id: string) => {
   // Get the screen width and height
@@ -72,6 +75,8 @@ const openQuizInNewWindow = (title: string, _id: string) => {
 
 const QuizCard = (props: QuizData<string | QuizQuestionsType>) => {
   const user = UseGetUserDataHook();
+  const [getResult, { data }] = useGetResultMutation();
+
   const handleQuizStart = (_id: string, title: string) => {
     openQuizInNewWindow(title, _id);
   };
@@ -84,8 +89,10 @@ const QuizCard = (props: QuizData<string | QuizQuestionsType>) => {
   const afterOpenModel = () => {
     console.log("model open");
   };
-  const handleViewResult = () => {
+  const handleViewResult = async () => {
     // window.open(`/quizes/result/${_id}`, "_blank", "noopener,noreferrer");
+    const res = await getResult({ quizId: props._id, userId: user.data._id });
+    console.log(res);
     setIsOpen(true);
   };
   const closeModal = () => setIsOpen(false);
@@ -95,7 +102,7 @@ const QuizCard = (props: QuizData<string | QuizQuestionsType>) => {
       <Card className="w-[400px] min-h-[280px] rounded-2xl  py-2 border border-bordergray flex flex-col justify-between">
         <CardHeader className="flex flex-col gap-4">
           <CardTitle className="text-darkcerulean">{props.title}</CardTitle>
-          <CardDescription className="text-darkcerulean ">
+          <CardDescription className="text-darkcerulean">
             {props.description}
           </CardDescription>
         </CardHeader>
@@ -164,18 +171,53 @@ const QuizCard = (props: QuizData<string | QuizQuestionsType>) => {
           </div>
         </CardFooter>
       </Card>
+
       <Modal
         isOpen={isOpen}
         onAfterOpen={afterOpenModel}
         onRequestClose={closeModal}
-        className="bg-white p-6 rounded-2xl shadow-lg max-w-lg mx-auto my-10 relative"
+        className="bg-white p-6 rounded-2xl shadow-lg max-w-lg mx-auto my-10 relative flex flex-col gap-6 w-[28rem]"
         overlayClassName="fixed w-screen h-screen inset-0 bg-black bg-opacity-50 flex justify-center items-center"
       >
-        <h2 className="text-xl font-bold mb-4">Standing</h2>
-        <p className="text-gray-700">
-          This is a modal styled with Tailwind CSS!
-        </p>
-        <button onClick={closeModal} className=" absolute right-2 top-2 ">
+        <div className="text-2xl text-center mb-4 tracking-widest font-montserrat flex items-center justify-center gap-2">
+          <span className="uppercase font-semibold border-b border-black">
+            Standings
+          </span>
+          <FaTrophy className="text-[#F7E383] text-4xl" />
+        </div>
+        <div className="mx-auto p-4 rounded-2xl border border-[#9A4D45] text-2xl">
+          <p>
+            Rank : {data?.data.standing.current_rank}/
+            {data?.data.standing.out_of}
+          </p>
+          <p>Percentile : {data?.data.percentile_obtained}</p>
+        </div>
+        <div className="p-4 bg-jetstream text-lg flex flex-col gap-2 rounded-2xl border border-[#555555]">
+          <p className="font-semibold text-[#22577A]">
+            Attempted :{" "}
+            {data?.data.report.length - data?.data.result.unattempted}/
+            {data?.data.report.length}
+          </p>
+          <p className="font-semibold text-[#22577A]">
+            Marks Obtained : {data?.data.result.marks_obtained}
+          </p>
+          <div className="flex gap-4">
+            <span className="text-[#3AA074]">
+              Correct : {data?.data.result.correct}/{data?.data.report.length}
+            </span>
+            <span className="text-[#9A4D45]">
+              Wrong : {data?.data.result.wrong}/{data?.data.report.length}
+            </span>
+          </div>
+        </div>
+        <Button
+          className="mx-auto rounded-full text-lg flex gap-2 border border-bordergray bg-[#F5F5F5] text-black hover:bg-slate-100"
+          size={"lg"}
+        >
+          <IoAnalytics />
+          Analysis
+        </Button>
+        <button onClick={closeModal} className="absolute right-2 top-2 ">
           <FaTimes />
         </button>
       </Modal>
